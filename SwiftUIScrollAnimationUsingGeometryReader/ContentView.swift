726ff260e472876335f04696314ee8057a4ab8f8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-07 08:35:57 +0300

    init

--- SwiftUIScrollAnimationUsingGeometryReader/ContentView.swift
+++ SwiftUIScrollAnimationUsingGeometryReader/ContentView.swift
@@ -10,7 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 50) {
+                ForEach(1..<5) { item in
+                    GeometryReader { geometry in
+                        Image("\(item)")
+                        .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                            .shadow(color: Color.gray, radius: 10, x: 0, y: 0)
+                            .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 20) / 4), axis: (x: 20, y: 20, z: 20))
+                    }
+                    .frame(width: 370, height: 400)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
